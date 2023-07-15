9ab48b3fe0624b7bda6fc872db3cb9f4cf256d0e
Jason M Graham <prof.jason.m.graham@gmail.com>
2023-07-15 13:17:56 -0400

    update

---
+++ R/boston_home_weather_eda.R
@@ -0,0 +1,53 @@
+# load packages
+library(tidyverse) # wrangling, plots etc.
+library(GGally) # paired plots
+library(skimr) # data summary
+
+# set plot theme
+theme_set(theme_minimal(base_size = 12))
+
+# load data
+boston_game_weather <- read_csv("data/boston_game_weather.csv")
+
+# glimpse
+glimpse(boston_game_weather)
+
+# overall data summary
+skim(boston_game_weather)
+
+# some manipulations
+boston_game_weather <- boston_game_weather %>%
+  mutate(year=year(date),
+         month=month(date),
+         day=day(date),
+         wl_binary=ifelse(wl=="W",1,0))
+
+# check results
+glimpse(boston_game_weather)
+
+# initial plots
+boston_game_weather %>%
+  ggplot(aes(x=hm_runs)) + 
+  geom_histogram()
+
+boston_game_weather %>%
+  ggplot(aes(x=wl)) + 
+  geom_bar()
+
+boston_game_weather %>%
+  ggplot(aes(x=duration)) + 
+  geom_histogram()
+
+boston_game_weather %>%
+  ggplot(aes(x=attendance)) + 
+  geom_histogram()
+
+boston_game_weather %>%
+  ggplot(aes(x=humid)) + 
+  geom_histogram()
+
+boston_game_weather %>%
+  ggplot(aes(x=tempF)) + 
+  geom_histogram()
+
+
