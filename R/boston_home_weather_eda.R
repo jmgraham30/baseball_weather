87df0936456d6d264fc0f964a9156fc65dd2f5ce
Jason M Graham <prof.jason.m.graham@gmail.com>
2023-07-31 11:49:16 -0400

    update

--- R/boston_home_weather_eda.R
+++ R/boston_home_weather_eda.R
@@ -246,3 +246,151 @@ boston_game_weather %>%
   geom_point(size=2,color="lightblue",alpha=0.5) + 
   geom_smooth(color="#512d6d",fill="darkgreen")
 
+hs_date <- boston_game_weather %>%
+  ggplot(aes(x=date,y=hm_runs)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Date",y="Red Sox Runs")
+hs_temp <- boston_game_weather %>%
+  ggplot(aes(x=tempF,y=hm_runs)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Temperature (F)",y="Red Sox Runs")
+hs_humid <- boston_game_weather %>%
+  ggplot(aes(x=humid,y=hm_runs)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Humidity",y="Red Sox Runs")
+hs_precip <- boston_game_weather %>%
+  ggplot(aes(x=precip,y=hm_runs)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Precipitable Water",y="Red Sox Runs")
+
+(hs_date + hs_temp) / (hs_humid + hs_precip)
+
+ws_date <- boston_game_weather %>%
+  ggplot(aes(x=date,y=wl,color=wl)) + 
+  geom_point(size=2,alpha=0.2) + 
+  scale_color_manual(values = c("#E69F00","#009E73")) + 
+  theme(legend.position = "none") + 
+  labs(x="Date",y="Win/Not")
+ws_temp <- boston_game_weather %>%
+  ggplot(aes(x=tempF,y=wl,color=wl)) + 
+  geom_point(size=2,alpha=0.2) + 
+  scale_color_manual(values = c("#E69F00","#009E73")) +
+  labs(x="Temperature (F)",y="Win/Not", color="Win/Not")
+ws_humid <- boston_game_weather %>%
+  ggplot(aes(x=humid,y=wl,color=wl)) + 
+  geom_point(size=2,alpha=0.2) + 
+  scale_color_manual(values = c("#E69F00","#009E73")) +
+  theme(legend.position = "none") +
+  labs(x="Humidity",y="Win/Not")
+ws_precip <- boston_game_weather %>%
+  ggplot(aes(x=precip,y=wl,color=wl)) + 
+  geom_point(size=2,alpha=0.2) + 
+  scale_color_manual(values = c("#E69F00","#009E73")) +
+  theme(legend.position = "none") +
+  labs(x="Precipitable Water",y="Win/Not")
+
+(ws_date + ws_temp) / (ws_humid + ws_precip)
+
+ds_date <- boston_game_weather %>%
+  ggplot(aes(x=date,y=duration)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Date",y="Game Duration")
+ds_temp <- boston_game_weather %>%
+  ggplot(aes(x=tempF,y=duration)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Temperature (F)",y="Game Duration")
+ds_humid <- boston_game_weather %>%
+  ggplot(aes(x=humid,y=duration)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Humidity",y="Game Duration")
+ds_precip <- boston_game_weather %>%
+  ggplot(aes(x=precip,y=duration)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Precipitable Water",y="Game Duration")
+
+(ds_date + ds_temp) / (ds_humid + ds_precip)
+
+as_date <- boston_game_weather %>%
+  filter(attendance > 0) %>%
+  ggplot(aes(x=date,y=attendance)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Date",y="Game Attendance")
+as_temp <- boston_game_weather %>%
+  filter(attendance > 0) %>%
+  ggplot(aes(x=tempF,y=attendance)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Temperature (F)",y="Game Attendance")
+as_humid <- boston_game_weather %>%
+  filter(attendance > 0) %>%
+  ggplot(aes(x=humid,y=attendance)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Humidity",y="Game Attendance")
+as_precip <- boston_game_weather %>%
+  filter(attendance > 0) %>%
+  ggplot(aes(x=precip,y=attendance)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Precipitable Water",y="Game Attendance")
+
+(as_date + as_temp) / (as_humid + as_precip)
+
+boston_game_weather %>%
+  group_by(year) %>%
+  summarise(wins_by_year=sum(wl_binary)) %>%
+  ggplot(aes(x=year,y=wins_by_year)) + 
+  geom_point(size=2,color="darkgreen") + 
+  geom_smooth(color="#512d6d",fill="lightblue") + 
+  labs(x="Year",y="Number of Wins")
+
+wa_temp <- boston_game_weather %>%
+  ggplot(aes(x=date,y=tempF)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Date",y="Temperature")
+wb_temp <- boston_game_weather %>%
+  group_by(year) %>%
+  summarise(mean_temp=mean(tempF)) %>%
+  ggplot(aes(x=year,y=mean_temp)) + 
+  geom_point(size=2,color="darkgreen",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="lightblue") + 
+  labs(x="Year",y="Mean Temperature")
+
+wa_humid <- boston_game_weather %>%
+  ggplot(aes(x=date,y=humid)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Date",y="Humidity")
+wb_humid <- boston_game_weather %>%
+  group_by(year) %>%
+  summarise(mean_humid=mean(humid)) %>%
+  ggplot(aes(x=year,y=mean_humid)) + 
+  geom_point(size=2,color="darkgreen",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="lightblue") + 
+  labs(x="Year",y="Mean Humidity")
+
+wa_precip <- boston_game_weather %>%
+  ggplot(aes(x=date,y=precip)) + 
+  geom_point(size=2,color="lightblue",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="darkgreen") + 
+  labs(x="Date",y="Percipitable Water")
+wb_precip <- boston_game_weather %>%
+  group_by(year) %>%
+  summarise(mean_precip=mean(precip)) %>%
+  ggplot(aes(x=year,y=mean_precip)) + 
+  geom_point(size=2,color="darkgreen",alpha=0.5) + 
+  geom_smooth(color="#512d6d",fill="lightblue") + 
+  labs(x="Year",y="Mean Percipitable Water")
+
+(wa_temp + wb_temp) / (wa_humid + wb_humid) / (wa_precip + wb_precip)
+
