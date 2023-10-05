9c9e73f16e74c02e741e22c5d5ab78a83b4ad9b9
Jason M Graham <prof.jason.m.graham@gmail.com>
2023-10-05 09:07:27 -0400

    add lr model for win/loss

--- R/runs_and_wins_models.R
+++ R/runs_and_wins_models.R
@@ -1,10 +1,14 @@
 # load useful packages
 library(tidyverse)
 library(tidymodels)
+library(parttree)
+library(vip)
+
 
 # set protocols and themes
 theme_set(theme_minimal(base_size = 12))
 tidymodels_prefer()
+doParallel::registerDoParallel()
 
 # load the data
 boston_game_weather <- read_csv("data/boston_game_weather.csv")
@@ -22,3 +26,35 @@ boston_game_weather <- boston_game_weather %>%
 
 # check results
 glimpse(boston_game_weather)
+
+
+### Models for Wins/Losses
+
+fit_lr_boots <- function(split){
+  glm(wl_binary ~ date + vis_runs + humid + precip + tempF,
+      family = binomial,
+      data=analysis(split))
+}
+
+boots <- bootstraps(boston_game_weather, times = 2000, strata = year)
+
+boot_models <-
+  boots %>% 
+  mutate(model = map(splits, fit_lr_boots),
+         coef_info = map(model, tidy))
+
+boot_coefs <- 
+  boot_models %>% 
+  unnest(coef_info)
+
+percentile_intervals <- int_pctl(boot_models, coef_info)
+
+ggplot(boot_coefs, aes(estimate)) +
+  geom_histogram(bins = 30,color="white") +
+  facet_wrap( ~ term, scales = "free") +
+  geom_vline(aes(xintercept = .lower), data = percentile_intervals, col = "blue") +
+  geom_vline(aes(xintercept = .upper), data = percentile_intervals, col = "blue")
+
+
+
+
